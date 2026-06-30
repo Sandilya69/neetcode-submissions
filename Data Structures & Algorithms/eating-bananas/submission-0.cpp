b917b6666973b3edb54be075781b3750054d5522
Rituraj Tripathi <tdivyansh702@gmail.com>
2026-07-01 02:44:38 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+
+            long long totalhour = 0;
+            for(int pile : piles){
+                totalhour += (pile + mid -1)/mid;
+            }
+
+            if(totalhour <= h){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+        
+    }
+};
