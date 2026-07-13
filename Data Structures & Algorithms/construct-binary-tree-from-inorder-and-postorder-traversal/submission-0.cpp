87275b3ae435f0480ae07d7636458215290aa009
Rituraj Tripathi <tdivyansh702@gmail.com>
2026-07-13 21:32:54 +0530

    Add: construct-binary-tree-from-inorder-and-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/construct-binary-tree-from-inorder-and-postorder-traversal/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int postIdx;
+    int inIdx;
+
+    TreeNode* dfs(vector<int>& postorder, vector<int>& inorder, int limit){
+
+        if(postIdx < 0){
+            return NULL;
+        }
+
+        if(inorder[inIdx] == limit){
+            inIdx--;
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(postorder[postIdx--]);
+        root->right = dfs(postorder, inorder, root->val);
+        root->left = dfs(postorder, inorder, limit);
+
+        return root;
+
+       
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+        postIdx = postorder.size() - 1;
+        inIdx = inorder.size() - 1;
+
+        return dfs(postorder, inorder, numeric_limits<int>::max());
+        
+    }
+};
